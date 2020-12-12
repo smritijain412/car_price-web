fa2109d56507fad2edbbd97e5a953fe1bb09f867
smritijain412 <61602017+smritijain412@users.noreply.github.com>
2020-12-12 13:46:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,url_for
 import jsonify
 import requests
 import pickle
@@ -7,7 +7,12 @@ model = pickle.load(open('model1.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
-@app.route("/predict", methods=['POST'])
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+    
+@app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
 #1
@@ -17,19 +22,19 @@ def predict():
     #3
         Mileage=float(request.form['Mileage'])
 #4
-        Fuel_Type=request.form['Fuel_Type']
-        if(Fuel_Type=='Petrol'):
-                Fuel_Type=0
+        Fuel=request.form['Fuel']
+        if(Fuel=='Petrol'):
+                Fuel=0
         else:
-            Fuel_Type=1
+            Fuel=1
     #5
-        Transmission_Mannual=request.form['Transmission_Mannual']
-        if(Transmission_Mannual=='Mechanic'):
-            Transmission_Mannual=0
+        Transmission=request.form['Transmission']
+        if(Transmission=='Mechanic'):
+            Transmission=0
         else:
-            Transmission_Mannual=1
+            Transmission=1
 
-        prediction=model.predict([[Year,Mileage,Fuel_Type,Volume,Transmission_Mannual]])
+        prediction=model.predict([[Year,Mileage,Fuel,Volume,Transmission]])
         output=round(prediction[0])
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
@@ -38,7 +43,6 @@ def predict():
             
     else:
         return render_template('index.html')
-
 if __name__=="__main__":
     app.run(debug=True)
 
